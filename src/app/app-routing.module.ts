1c884ee0f67e31d80fd83cf6f9084cb4b4e1e9a9
Nazim Ahmed <hoquerumon@yahoo.com>
2026-05-15 17:57:15 +0600

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'requestor',
+        redirectTo: 'auth/login',
         pathMatch: 'full'
       },
       {
